5bd8403241832451cee18be148eeb5bec10a3328
Panda Diestro <71461113+PandaDiestro@users.noreply.github.com>
2020-11-26 14:26:32 -0500

    mcd 2.0
    ahora los resultados son mas exactos y en numeros enteros (proximamente en factores primos tambien)

--- mcd.cpp
+++ mcd.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
+#include <cstdbool>
 using namespace std;
 
 int get_int(string string);
+int mcm(int a, int b);
 
 int main()
 {
-  int divisor = 2;
-  int counter = 0;
-  int m = get_int("Numero 1: ");
-  int n = get_int("Numero 2: ");
-  while (m > 1 && n > 1)
-  {
-    if(m % divisor == 0 && n % divisor == 0 )
-    {
-      m /= divisor;
-      n /= divisor;
-      counter++;
-      if (m % divisor != 0 && n % divisor != 0 )
-      {
-        cout<<divisor<<"^"<<counter<<"x";
-      }
-    }
-    else
-    {
-      divisor++;
-    }
-  }
-  cout<<"1\n";
+	int num1 = get_int("Numero 1: ");
+	int num2 = get_int("Numero 2: ");
+	bool over1 = false;
+	int mcd = (num1 * num2) / mcm(num1, num2);
+	cout<<mcd<<"\n";
 }
-
+	
 int get_int(string string)
 {
   int n;
   cout<<string;
   cin>>n;
   return n;
+}
+
+int mcm(int a, int b)
+{
+	int n1 = a;
+	int n2 = b;
+	bool over = false;
+	for(int mcm = 2; over == false; mcm++)
+	{
+		if(mcm % n1 == 0 && mcm % n2 == 0)
+		{
+			over = true;
+			return mcm;
+		}
+	}
 }
